dc0153e830558771343e6f7467980704275ef7ef
Tolganay <toki-tok9900@mail.ru>
2022-10-30 02:58:34 +0600

    Homework 06

---
+++ HW06/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+/*int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Т.е. считываем строку с консоли Console.ReadLine(), потом методом Split() получаем из этой строки массив,
+ при этом при отсутствии параметров в качестве разделителей будут приняты все пробельные символы.
+
+Затем метод Array.ConvertAll применяет ко всем элементам входного массива функцию, 
+переданную вторым параметром (int.Parse), таким образом, из массива строк получается массив int
+*/
+
+
+/*
+Console.Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);  
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+*/
+
+
+/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+/*
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = (b2 - b1) / (k1 - k2);
+var y = k1 * x + b1;
+
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+*/
